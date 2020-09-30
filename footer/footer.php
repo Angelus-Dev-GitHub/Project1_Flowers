c2dd50d3b840aceaea7bcac9d32cced09c65643b
Marcel Lavergne <lavergne.marcel@gmail.com>
2020-09-30 14:54:25 +0200

    footer v1 php and css

---
+++ footer/footer.php
@@ -0,0 +1,67 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link type="text/css" rel="stylesheet" href="footer.css">
+</head>
+<body>
+    <main>
+    <footer class="footer">
+        <section class="footer_social_network">
+            <div class="footer_social_networks-message">
+            <h3>retrouvez nous sur <br> nos réseaux:</h3>
+            </div>
+            <div class="footer_social_network-instagram">
+                <img
+                src="https://cdn.pixabay.com/photo/2016/08/09/17/52/instagram-1581266_960_720.jpg"
+                alt="logo instagram"
+
+                    />
+                <p>Instagram</p>
+
+            </div>
+            <div class="footer_social_network-facebook" >
+                <img
+                src="https://cdn.pixabay.com/photo/2015/05/17/10/51/facebook-770688_960_720.png"
+                alt="logo facebook"
+
+                    />
+                <p>
+Facebook
+                </p>
+            </div>
+            <div class= "footer_social_network-twitter">
+                <img
+                src="https://cdn.pixabay.com/photo/2017/08/23/11/30/twitter-2672572_960_720.jpg"
+                alt="logo twitter"
+                    />
+                <p>Twitter</p>
+            </div>
+
+    </section>
+    <section class="footer_our_shops">
+        <div class="footer_our_shops-h3">
+            <h3>Nos magasins :</h3>
+        </div>
+        <div class="footer_our_shops-p">
+            <p>6 avenue thiers, Bordeaux <br> 1 Bis allée du 8 mai 1945, Gaillan-en-Médoc</p>
+        </div>
+    </section>
+    <section class="footer_about_us">
+        <div class="footer_about_us-table">
+            <ul>
+
+               <li>F.A.Q</li>
+               <li>à propos de nous</li>
+               <li>Nous contacter</li>
+            </ul>
+        </div>
+
+    </section>
+    </footer>
+</main>
+</body>
+</html>
