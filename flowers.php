308df734fae482855ff3479bb222ccfebb1ea4c9
kozmarti <kozmarti@gmail.com>
2020-10-01 16:46:27 +0200

    navigation and flowers

---
+++ flowers.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="flowersstyle.css">
+    <link rel="stylesheet" href="nav.css">
+    <link href="https://fonts.googleapis.com/css2?family=Calligraffitti&family=Raleway&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<div class="page-container">
+<?php include("nav.php"); ?>
+    <main>
+        <div class="page-container">
+            <h1 class="title-of-the-page"> Boutique en ligne </h1>
+            <p class="flowers_subtitle">Composez votre propre bouquet !</p>
+            <section class="flowers_container">
+                <div class="flowers_rose flower_unite-container" data-aos="zoom-in">
+                    <img src="https://images.unsplash.com/photo-1576857990591-aad2585f34d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="">
+                    <div class="flowers_text">
+                        <h2> Rose </h2>
+                        <p>Prix: <b>€2.99</b></p>
+                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
+                        <button>Ajouter au panier</button>
+                    </div>
+                </div>
+                <div class="flowers_daffodil flower_unite-container" data-aos="zoom-in">
+                    <img src="https://images.unsplash.com/photo-1547606373-7097fb4b75d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+                    <div class="flowers_text">
+                        <h2>Jonquille</h2>
+                        <p>Prix: <b>€3.99</b></p>
+                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
+                        <button>Ajouter au panier</button>
+                    </div>
+                </div>
+                <div class="flowers_anemone flower_unite-container" data-aos="zoom-in">
+                    <img src="https://images.unsplash.com/photo-1589744534282-f1f71d5e3616?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=661&q=80" alt="">
+                    <div class="flowers_text">
+                        <h2>Anémone</h2>
+                        <p>Prix: <b>€2.49</b></p>
+                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
+                        <button>Ajouter au panier</button>
+
+                    </div>
+                </div>
+                <div class="flowers_carnation flower_unite-container" data-aos="zoom-in">
+                    <img src="https://images.unsplash.com/photo-1570631810442-f20e700b0ce1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="">
+                    <div class="flowers_text">
+                        <h2> Œillet</h2>
+                        <p>Prix: <b>€4.99</b></p>
+                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
+                        <button>Ajouter au panier</button>
+                    </div>
+                </div>
+                <div class="flowers_anthurium flower_unite-container" data-aos="zoom-in">
+                    <img src="https://images.unsplash.com/photo-1548031101-42105b053869?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+                    <div class="flowers_text">
+                        <h2>Anthurium</h2>
+                        <p>Prix: <b>€3.99</b></p>
+                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
+                        <button>Ajouter au panier</button>
+                    </div>
+                </div>
+                <div class="flowers_gladiolus flower_unite-container" data-aos="zoom-in">
+                    <img src="https://images.unsplash.com/photo-1525310072745-f49212b5ac6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80" alt="">
+                    <div class="flowers_text">
+                        <h2>Glaïeul</h2>
+                        <p>Prix: <b>€3.49</b></p>
+                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
+                        <button>Ajouter au panier</button>
+                    </div>
+                </div>
+                <div class="flowers_lily flower_unite-container" data-aos="zoom-in">
+                    <img src="https://images.unsplash.com/photo-1502977249166-824b3a8a4d6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt="">
+                    <div class="flowers_text">
+                        <h2>Lys</h2>
+                        <p>Prix: <b>€1.99</b></p>
+                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
+                        <button>Ajouter au panier</button>
+                    </div>
+                </div>
+                <div class="flowers_tulip flower_unite-container" data-aos="zoom-in">
+                    <img src="https://images.unsplash.com/flagged/photo-1561023368-08a67ce21a09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1358&q=80" alt="">
+                    <div class="flowers_text">
+                        <h2>Tulipe</h2>
+                        <p>Prix: <b>€4.49</b></p>
+                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
+                        <button>Ajouter au panier</button>
+                    </div>
+                </div>
+                <div class="flowers_orchid flower_unite-container" data-aos="zoom-in">
+                    <img src="https://images.unsplash.com/photo-1545818754-87cb4616981b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80" alt="">
+                    <div class="flowers_text">
+                        <h2>Orchidée</h2>
+                        <p>Prix: <b>€3.99</b></p>
+                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
+                        <button>Ajouter au panier</button>
+                    </div>
+                </div>
+
+            </section>
+        </div>
+    </main>
+</div>
+<script src="nav.js"></script>
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
+</body>
+
+</html>
