1c25864c69eeb3b597e851e80992fef542fbd33b
Angelus-Dev-GitHub <t.dufosse666@gmail.com>
2020-10-01 15:59:13 +0200

    Create index + shop

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_index.css">
+    <title>D'une fleur à l'autre</title>
+    <<style>
+        @import url('https://fonts.googleapis.com/css2?family=Calligraffitti&family=Raleway&display=swap');
+    </style>
+</head>
+<body>
+<main>
+    <header>
+        <?php include 'nav.php'; ?>
+
+    </header>
+    <section class="index_section">
+       
+    <h1 class="title-of-the-page">D'une fleur à l'autre</h1>
+    
+
+    <div class="index_banner">
+        <p>Fleuriste situé en plein coeur de Bordeaux Bastide.<br>Compositions par évènements ou à la carte<br>Possibilité de cartes personnalisées.<br>Livraison possible.</p>
+
+    </div>
+    <h2>Nos dernières compositions</h2>
+
+
+    <footer>
+        <?php include 'footer.php'; ?>
+
+    </footer>
+        
+    </section>
+</main>
+</body>
+</html>
