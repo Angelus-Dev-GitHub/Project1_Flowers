a363ad7fff5834ce5427c8709730fbe9154acf36
Marcel Lavergne <lavergne.marcel@gmail.com>
2020-10-02 19:00:28 +0200

    obligation de remplir les champs dans la section contact

--- contact.php
+++ contact.php
@@ -53,34 +53,34 @@
                         alt="icone représentant les emails"
                     />
                     <p>dunefleuralautre@gmail.com</p>
-                </div>
-        </div>
+                    </div>
             </div>
-                <div class="contact_contact_us-Ou">
-                    <h3>
-                        Ou
-                    </h3>
                 </div>
-                <div class="contact_contact_us-introduction-formulaire">
-                    <h2>Ecrivez nous</h2>
-                </div>
-                <div class="contact_contact_us-formulaire">
-                    <form id="contactForm">
+                    <div class="contact_contact_us-Ou">
+                        <h3>
+                            Ou
+                        </h3>
+                    </div>
+                    <div class="contact_contact_us-introduction-formulaire">
+                        <h2>Ecrivez nous</h2>
+                    </div>
+                    <div class="contact_contact_us-formulaire">
+                        <form id="contactForm" ">
 
-                        <label  for="nom">Nom :</label>
-                        <input  type="text"  id="nom"  name="user_name">
+                            <label  for="nom">Nom :</label>
+                            <input  type="text"  id="nom"  name="user_name" required>
 
-                        <label for="firstName">Prénom :</label>
-                        <input type="text" id="prénom" name="user_firstName">
+                            <label for="firstName">Prénom :</label>
+                            <input type="text" id="firstName" name="user_firstName" required>
 
-                        <label  for="courriel">Courriel :</label>
-                        <input  type="email"  id="courriel"  name="user_email">
+                            <label  for="courriel">Courriel :</label>
+                            <input  type="email"  id="courriel"  name="user_email" required>
 
-                        <label for= "phone">Téléphone  : </label>
-                        <input type= "text" id="phoneNumber" name="user_phone">
+                            <label for= "phone">Téléphone  : </label>
+                            <input type= "text" id="phone" name="user_phone" required>
 
-                        <label  for="message">Message :</label>
-                        <textarea  id="message"  name="user_message"></textarea>
+                            <label  for="message">Message :</label>
+                        <textarea  id="message"  name="user_message" required></textarea>
 
                         <div class="contact_contact_us_formulaire-option">
                                     <label for="select">Sujet </label>
@@ -97,7 +97,7 @@
                             <input type="submit" value="Submit">
                             <script src="contact.js"></script>
                         </div>
-                    </form>
+                        </form>
         </div>
     </section>
 <footer>
