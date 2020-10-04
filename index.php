526ae8cb9403eb14d21ba3f6f1f3d40a93b3fbd8
Angelus-Dev-GitHub <t.dufosse666@gmail.com>
2020-10-04 21:29:43 +0200

    Modif index.php composition + responsive

--- index.php
+++ index.php
@@ -17,20 +17,33 @@
 <?php include 'nav.php'; ?>
 
 <main>
-
-
-
     <section class="index_section">
-       
-    <h1 class="title-of-the-page">D'une fleur à l'autre</h1>
-    
 
+    <h1 class="title-of-the-page">D'une fleur à l'autre</h1>
     <div class="index_banner">
         <p>Fleuriste situé en plein coeur de Bordeaux Bastide.<br>Compositions par évènements ou à la carte<br>Possibilité de cartes personnalisées.<br>Livraison possible.</p>
 
     </div>
-    <h2>Nos dernières compositions</h2>
+    <h2>Nos compositions d'Automne</h2>
+    <div class="index_composition">
+        <div class="index_composition_bouquet">
+            <img src="https://images.unsplash.com/photo-1524048914400-67614a52ffc9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="bouquet1">
+            <h3>Soleil d'Automne</h3>
+        </div>
+        <div class="index_composition_bouquet">
+            <img src="https://images.unsplash.com/photo-1533616688419-b7a585564566?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="bouquet2">
+            <h3>Ambre</h3>
+        </div>
+        <div class="index_composition_bouquet" >
+            <img src="https://images.unsplash.com/photo-1525096891900-65f12c04065f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="bouquet3">
+            <h3>Bouquet Champêtre</h3>
+        </div>
+        <div class="index_composition_bouquet">
+            <img src="https://images.unsplash.com/photo-1511690005878-e66ce2772f3a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="bouquet4">
+            <h3>Bouquet Sauvage</h3>
+        </div>
 
+    </div>
 
     <footer>
         <?php include './footer/footer.php'; ?>
