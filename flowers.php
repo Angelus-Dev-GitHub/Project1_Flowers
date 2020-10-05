06c9390f897484dcfc443f2dff3b897cbb487e37
kozmarti <kozmarti@gmail.com>
2020-10-05 22:36:35 +0200

    online purchase basket JS file added and flowers page CSS developed

--- flowers.php
+++ flowers.php
@@ -25,8 +25,8 @@
                     <div class="flowers_text">
                         <h2> Rose </h2>
                         <p>Prix: <b>€2.99</b></p>
-                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
-                        <button>Ajouter au panier</button>
+
+                        <button id="rose-button">Ajouter au panier</button>
                     </div>
                 </div>
                 <div class="flowers_daffodil flower_unite-container" data-aos="zoom-in">
@@ -34,8 +34,8 @@
                     <div class="flowers_text">
                         <h2>Jonquille</h2>
                         <p>Prix: <b>€3.99</b></p>
-                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
-                        <button>Ajouter au panier</button>
+
+                        <button id="jon-button">Ajouter au panier</button>
                     </div>
                 </div>
                 <div class="flowers_anemone flower_unite-container" data-aos="zoom-in">
@@ -43,8 +43,8 @@
                     <div class="flowers_text">
                         <h2>Anémone</h2>
                         <p>Prix: <b>€2.49</b></p>
-                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
-                        <button>Ajouter au panier</button>
+
+                        <button id="anem-button">Ajouter au panier</button>
 
                     </div>
                 </div>
@@ -53,8 +53,8 @@
                     <div class="flowers_text">
                         <h2> Œillet</h2>
                         <p>Prix: <b>€4.99</b></p>
-                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
-                        <button>Ajouter au panier</button>
+
+                        <button id="oeil-button">Ajouter au panier</button>
                     </div>
                 </div>
                 <div class="flowers_anthurium flower_unite-container" data-aos="zoom-in">
@@ -62,8 +62,8 @@
                     <div class="flowers_text">
                         <h2>Anthurium</h2>
                         <p>Prix: <b>€3.99</b></p>
-                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
-                        <button>Ajouter au panier</button>
+
+                        <button id="ant-button">Ajouter au panier</button>
                     </div>
                 </div>
                 <div class="flowers_gladiolus flower_unite-container" data-aos="zoom-in">
@@ -71,8 +71,8 @@
                     <div class="flowers_text">
                         <h2>Glaïeul</h2>
                         <p>Prix: <b>€3.49</b></p>
-                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
-                        <button>Ajouter au panier</button>
+
+                        <button id="gla-button">Ajouter au panier</button>
                     </div>
                 </div>
                 <div class="flowers_lily flower_unite-container" data-aos="zoom-in">
@@ -80,8 +80,8 @@
                     <div class="flowers_text">
                         <h2>Lys</h2>
                         <p>Prix: <b>€1.99</b></p>
-                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
-                        <button>Ajouter au panier</button>
+
+                        <button id="lys-button">Ajouter au panier</button>
                     </div>
                 </div>
                 <div class="flowers_tulip flower_unite-container" data-aos="zoom-in">
@@ -89,8 +89,8 @@
                     <div class="flowers_text">
                         <h2>Tulipe</h2>
                         <p>Prix: <b>€4.49</b></p>
-                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
-                        <button>Ajouter au panier</button>
+
+                        <button id="tul-button">Ajouter au panier</button>
                     </div>
                 </div>
                 <div class="flowers_orchid flower_unite-container" data-aos="zoom-in">
@@ -98,18 +98,37 @@
                     <div class="flowers_text">
                         <h2>Orchidée</h2>
                         <p>Prix: <b>€3.99</b></p>
-                        <label for="flower_quantity">Quantité : </label><input class=flowers_counter type="number" />
-                        <button>Ajouter au panier</button>
+
+                        <button id="orch-button" >Ajouter au panier</button>
                     </div>
                 </div>
+                <section class="basket">
+                    <h2 >Votre panier</h2>
+                    <ul id="basket-list">
+                        <li id="rose-basketitem"><span id="rose-delete" class="delete-button"> X </span><b>Rose</b> :  <span id="rose-quantity"> 0 </span> ......................€ <span id="rose-amount"> 0 </span> </li>
+                        <li id="jon-basketitem"> <span id="jon-delete" class="delete-button"> X </span> <b>Jonquille </b> :  <span id="jon-quantity"> 0 </span> ......................€ <span id="jon-amount"> 0 </span> </li>
+                        <li id="anem-basketitem"> <span id="anem-delete" class="delete-button"> X </span><b>Anémone </b> :  <span id="anem-quantity"> 0 </span> ......................€ <span id="anem-amount"> 0 </span> </li>
+                        <li id="oeil-basketitem"><span id="oeil-delete" class="delete-button"> X </span> <b>Œillet </b> :  <span id="oeil-quantity"> 0 </span> ......................€ <span id="oeil-amount"> 0 </span> </li>
+                        <li id="ant-basketitem"> <span id="ant-delete" class="delete-button"> X </span><b>Anthurium</b> :  <span id="ant-quantity"> 0 </span> ......................€ <span id="ant-amount"> 0 </span> </li>
+                        <li id="gla-basketitem"> <span id="gla-delete" class="delete-button"> X </span><b>Glaïeul </b> :  <span id="gla-quantity"> 0 </span> ......................€ <span id="gla-amount"> 0 </span> </li>
+                        <li id="lys-basketitem"> <span id="lys-delete" class="delete-button"> X </span><b>Lys </b> :  <span id="lys-quantity"> 0 </span> ......................€ <span id="lys-amount"> 0 </span> </li>
+                        <li id="tul-basketitem"> <span id="tul-delete" class="delete-button"> X </span><b>Tulipe </b> :  <span id="tul-quantity"> 0 </span> ......................€ <span id="tul-amount"> 0 </span> </li>
+                        <li id="orch-basketitem"><span id="orch-delete" class="delete-button"> X </span><b>Orchidée</b> :  <span id="orch-quantity"> 0 </span> ......................€ <span id="orch-amount"> 0 </span> </li>
 
+                    </ul>
+
+                    <p class="basket-total"><b>TOTAL : €<span id="total-amount">  0 </span></b></p>
+                    <button>Commande</button>
+                </section>
             </section>
+
         </div>
         <footer>
             <?php include './footer/footer.php'; ?>
         </footer>
     </main>
 </div>
+<script src="flowers.js"></script>
 <script src="nav.js"></script>
 <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
 <script>
